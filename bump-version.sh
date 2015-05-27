0233ed5fd810be91f2304c78576d254877f649b3
Slavomir Vojacek <slavomirvojacek@gmail.com>
2015-05-27 12:47:58 +0100

    edit bump-version

--- bump-version.sh
+++ bump-version.sh
@@ -4,17 +4,14 @@ printf "\n\n\nDo you wish to bump current version to ${1}?\n\nThe version bump m
 select yn in "Yes" "No"; do
 	case $yn in
 		Yes )
-				bower version {$1}
-                npm version {$1}
+			bower version $1
+                	npm version $1
 
-                git tag -a $1 -m "${2}"
-                git push --tags
+                	printf "\n\n\nVersion has been bumped to {$1}\n\n"
 
-                printf "\n\n\nVersion has been bumped to {$1}\n\n"
-
-				break;;
+			break;;
 		No )
-				echo "\n\n\nOk, maybe next time :-)\n\n"
-				exit;;
+			echo "\n\n\nOk, maybe next time :-)\n\n"
+			exit;;
 	esac
 done
