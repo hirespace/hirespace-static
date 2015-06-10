53f9e276aeb0934f3489410c8866ec9299f28cfa
Slavomir Vojacek <slavomirvojacek@gmail.com>
2015-06-10 17:53:16 +0100

    HS-2542 added View Model

---
+++ js/shared/View/View.specs.ts
@@ -0,0 +1,81 @@
+module hirespace.specs {
+    'use strict';
+
+    let fakeMetaData = {
+        valid: [
+            {
+                testSentence: "show: bookingData.stage == 'In Progress'",
+                action: 'show',
+                assertion: {
+                    object: 'bookingData.stage',
+                    sentence: "bookingData.stage == 'In Progress'",
+                    type: 'equality',
+                    value: 'In Progress'
+                }
+            },
+            {
+                testSentence: "show: bookingData.status=='Closed'",
+                action: 'show',
+                assertion: {
+                    object: 'bookingData.status',
+                    sentence: "bookingData.status=='Closed'",
+                    type: 'equality',
+                    value: 'Closed'
+                }
+            },
+            {
+                testSentence: "hide:bookingData.budget =='3000'",
+                action: 'hide',
+                assertion: {
+                    object: 'bookingData.budget',
+                    sentence: "bookingData.budget =='3000'",
+                    type: 'equality',
+                    value: '3000'
+                }
+            },
+            {
+                testSentence: "show : bookingData._id",
+                action: 'show',
+                assertion: {
+                    object: 'bookingData._id',
+                    sentence: "bookingData._id",
+                    type: 'boolean',
+                    value: true
+                }
+            },
+            {
+                testSentence: "hide :bookingData.venue.name",
+                action: 'hide',
+                assertion: {
+                    object: 'bookingData.venue.name',
+                    sentence: "bookingData.venue.name",
+                    type: 'boolean',
+                    value: true
+                }
+            }
+        ],
+        invalid: []
+    };
+
+    describe('View module', () => {
+        _.forEach(fakeMetaData.valid, (data) => {
+            let sentence = data.assertion.sentence;
+            let assertion = hirespace.View.parseAssertionSentence(sentence);
+
+            it('should parse assertion sentence: ' + sentence, () => {
+                expect(assertion.object).toEqual(data.assertion.object);
+                expect(assertion.type).toEqual(data.assertion.type);
+                expect(assertion.value).toEqual(data.assertion.value);
+            });
+
+            it('should successfully decrypt assertion meta data', () => {
+                _.forEach(fakeMetaData.valid, (data) => {
+                    let assertionData: IAssertionMetaData = hirespace.View.parseAssertionMetaData(data.testSentence);
+
+                    expect(assertionData.action).toEqual(data.action);
+                    expect(_.isEqual(assertionData.assertion, data.assertion)).toBe(true);
+                });
+            });
+        });
+    });
+}
---
+++ js/shared/View/View.ts
@@ -0,0 +1,40 @@
+module hirespace {
+    'use strict';
+
+    export interface IAssertionSentence {
+        object: string;
+        sentence: string;
+        type: string;
+        value: string | boolean;
+    }
+
+    export interface IAssertionMetaData {
+        action: string;
+        assertion: IAssertionSentence
+    }
+
+    export class View {
+
+        static parseAssertionMetaData(assertion: string): IAssertionMetaData {
+            let assertionSplit = assertion.split(':');
+
+            return {
+                action: _.trim(_.first(assertionSplit)),
+                assertion: hirespace.View.parseAssertionSentence(_.last(assertionSplit))
+            };
+        }
+
+        static parseAssertionSentence(assertionString: string): IAssertionSentence {
+            let assertion = _.trim(assertionString),
+                splitAssertion = assertion.split('=='),
+                type: string = splitAssertion.length == 2 ? 'equality' : 'boolean';
+
+            return {
+                object: _.trim(_.first(splitAssertion)),
+                sentence: assertion,
+                type: type,
+                value: type == 'equality' ? _.trim(_.last(splitAssertion), ' \' ') : true
+            }
+        }
+    }
+}
