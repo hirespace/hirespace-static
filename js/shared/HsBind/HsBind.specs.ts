29d7799fc4f59affaaa42a80be8a7fc2b7d22721
Slavomir Vojacek <slavomirvojacek@gmail.com>
2015-06-24 13:51:44 +0100

    HS-2542 HsBind new scenarios to tests

--- js/shared/HsBind/HsBind.specs.ts
+++ js/shared/HsBind/HsBind.specs.ts
@@ -3,7 +3,8 @@ module hirespace.specs {
 
     describe('HsBind model', () => {
         let fakeResolveObject = {
-            fakeData: true
+            boolean: true,
+            zero: 0
         };
 
         // @TODO
@@ -15,23 +16,23 @@ module hirespace.specs {
             strong: true
         };
 
-        _.forEach(elementTagNames, (pair, tag) =>
-            $('body').append('<' + tag + ' class="elem" hs-bind="fakeData">' + (pair ? '</' + tag + '>' : '')));
+        _.forEach(fakeResolveObject, (value, key) => {
+            _.forEach(elementTagNames, (pair, tag) =>
+                $('body').append('<' + tag + ' class="' + key + '" hs-bind="' + key + '">' + (pair ? '</' + tag + '>' : '')));
 
-        let fakeElems = $('body .elem');
+            _.forEach($('body .' + key), elem => {
+                it('should correctly resolve the object value and render it into ' + elem.tagName + ' element', () => {
+                    hirespace.HsBind.updateElem(elem, fakeResolveObject);
 
-        _.forEach(fakeElems, elem => {
-            it('should correctly resolve the object value and render it into ' + elem.tagName + ' element', () => {
-                hirespace.HsBind.updateElem(elem, fakeResolveObject);
-
-                switch(elem.tagName.toLowerCase()) {
-                    case 'input':
-                        expect($(elem).val()).toEqual(fakeResolveObject.fakeData.toString());
-                        break;
-                    default:
-                        expect($(elem).html()).toEqual(fakeResolveObject.fakeData.toString());
-                        break;
-                }
+                    switch (elem.tagName.toLowerCase()) {
+                        case 'input':
+                            expect($(elem).val()).toEqual(value.toString());
+                            break;
+                        default:
+                            expect($(elem).html()).toEqual(value.toString());
+                            break;
+                    }
+                });
             });
         });
     });
