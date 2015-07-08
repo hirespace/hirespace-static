7dcbbfb9f22483af3222680ed49ac04b82520054
Slavomir Vojacek <slavomirvojacek@gmail.com>
2015-07-08 10:11:37 +0100

    HS-2768 notification model finished, layouts updated

--- js/shared/Notification/Notification.specs.ts
+++ js/shared/Notification/Notification.specs.ts
@@ -2,8 +2,22 @@ module hirepace.specs {
     'use strict';
 
     describe('Notification model', () => {
+        $('body').append('<div id="notification" class="dummy-class"></div>');
+
         it('should have the generate() method', () => {
             expect(hirespace.Notification.generate).toBeDefined();
+        });
+
+        it('should reset classes attached to the notification element and set new classes onto it', () => {
+            hirespace.Notification.generate('Test', 'error');
+
+            expect($('#notification').attr('class')).toEqual('active error');
+        });
+
+        it('should render a main element with correct content into the notification element', () => {
+            hirespace.Notification.generate('Hello, World!', 'success');
+
+            expect($('#notification main').html()).toEqual('Hello, World!');
         });
     });
 }
--- js/shared/Notification/Notification.ts
+++ js/shared/Notification/Notification.ts
@@ -1,13 +1,17 @@
 module hirespace {
     'use strict';
 
+    let clearNotificationTimeout;
+
     export class Notification {
         static generate(message: string, type: string) {
             $('#notification').attr('class', '').addClass('active ' + type).html('<main>' + message + '</main>');
 
+            if (clearNotificationTimeout) clearTimeout(clearNotificationTimeout);
+
             // @TODO
             // abstract interval into a magic var
-            setInterval(() => {
+            clearNotificationTimeout = setTimeout(() => {
                 $('#notification').removeClass('active');
             }, 5000);
         }
