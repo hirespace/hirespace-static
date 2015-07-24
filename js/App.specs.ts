af9501a019a36ff9fd218a57712f5fe9c98c2fb2
Slavomir Vojacek <slavomirvojacek@gmail.com>
2015-07-24 10:37:10 +0100

    bump version and pre-release cleaning

--- js/App.specs.ts
+++ js/App.specs.ts
@@ -4,15 +4,10 @@ module hirespace.specs {
     describe('App', () => {
         afterEach(() => {
             hirespace.App.subscriptions = {};
-            hirespace.App.knockout = {};
         });
 
         it('should have subscriptions Object, where services / controllers will get registered', () => {
             expect(typeof hirespace.App.subscriptions).toEqual('object');
-        });
-
-        it('should have knockout Array, where knockout models will get registered', () => {
-            expect(typeof hirespace.App.knockout).toEqual('object');
         });
 
         it('should have subscribe method that registers functions to the subscriptions global', () => {
@@ -27,14 +22,6 @@ module hirespace.specs {
             hirespace.App.subscribe('bar', () => {
             });
             expect(_.keys(hirespace.App.subscriptions).length).toEqual(originalSubscriptionsLength + 2);
-        });
-
-        it('should have subscribe method that registers functions to the knockout global', () => {
-            let originalKnockoutLength = _.keys(hirespace.App.knockout).length;
-
-            hirespace.App.subscribe('foobar', () => {}, true);
-
-            expect(_.keys(hirespace.App.knockout).length).toEqual(originalKnockoutLength + 1);
         });
     });
 }
--- js/App.ts
+++ js/App.ts
@@ -8,25 +8,18 @@ module hirespace {
             [name: string]: any
         } = {};
 
-        static knockout: {
-            [name: string]: boolean
-        } = {};
-
-        static subscribe(name: string, func: any, knockout?: boolean) {
+        static subscribe(name: string, func: any) {
             hirespace.App.subscriptions[name] = func;
-
-            if (knockout) {
-                hirespace.App.knockout[name] = true;
-            }
         }
     }
 
     $(document).ready(() => {
-        $('body').removeClass('pre-load');
+        let body = $('body');
 
         hirespace.Logger.debug('Hire Space app now running');
 
-        let controller = $('body').attr('data-ctr');
+        body.removeClass('pre-load');
+        let controller = body.attr('data-ctr');
 
         hirespace.DropDown.listen();
 
@@ -42,11 +35,7 @@ module hirespace {
         });
 
         if (controller && hirespace.App.subscriptions[controller]) {
-            if (hirespace.App.knockout[controller]) {
-                ko.applyBindings(new hirespace.App.subscriptions[controller]);
-            } else {
-                new hirespace.App.subscriptions[controller];
-            }
+            new hirespace.App.subscriptions[controller];
         }
     });
 }
